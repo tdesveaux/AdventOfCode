5b0b89408f1492247eac387e5ff168490d7cb67c
Thomas Desveaux <desveaux.thomas@gmail.com>
2022-12-03 15:02:35 +0100

    [DAY03][Part Two] solution

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,9 +1,75 @@
 use std::{fs::File, io::{self, BufRead}, collections::HashSet};
 use clap::Parser;
+use itertools::Itertools;
+
+#[derive(clap::ValueEnum, Clone)]
+enum Mode {
+    Part01,
+    Part02,
+}
 
 #[derive(Parser)]
 struct Cli {
     path: std::path::PathBuf,
+
+    #[arg(value_enum)]
+    mode: Mode,
+}
+
+fn score_item(c: char) -> u32 {
+    match c {
+        'a'..='z' => (c as u32) - ('a' as u32) + 1,
+        'A'..='Z' => (c as u32) - ('A' as u32) + 27,
+        _ => panic!("Unexcpected byte in input")
+    }
+}
+
+fn part_01(fp: File) {
+    let mut total = 0;
+    for line in io::BufReader::new(fp).lines() {
+        let line = line.unwrap();
+        if line.len() % 2 != 0 {
+            panic!("Rucksack does not contains same number of items in each compartment");
+        }
+
+        let half_index = line.len() / 2;
+        let left_compartment: HashSet<char> = HashSet::from_iter(line[..half_index].chars());
+        let right_compartment: HashSet<char> = HashSet::from_iter(line[half_index..].chars());
+
+        let difference_sum: u32 = left_compartment
+            .intersection(&right_compartment)
+            .map(|c| score_item(*c))
+            .sum();
+
+        total += difference_sum;
+    }
+
+    println!("Total is {}", total);
+}
+
+fn part_02(fp: File) {
+    const GROUP_SIZE: usize = 3;
+
+    let lines = io::BufReader::new(fp)
+        .lines()
+        .map(|l| l.unwrap());
+
+    let mut total: u32 = 0;
+    for group in &lines.chunks(GROUP_SIZE) {
+
+        let chunk_intersection = group
+            .map(|c| HashSet::<char>::from_iter(c.chars()))
+            .reduce(|accum, item| &accum & &item)
+            .expect("Failed to find common tag");
+
+        if chunk_intersection.len() != 1 {
+            panic!("Found more than one common item");
+        }
+
+        total += chunk_intersection.iter().map(|c| score_item(*c)).sum::<u32>();
+    }
+
+    println!("Total is {}", total);
 }
 
 fn main() {
@@ -16,29 +82,8 @@ fn main() {
     }
 
     let fp = File::open(file_path).unwrap();
-
-    let mut total = 0;
-    for line in io::BufReader::new(fp).lines() {
-        let line = line.unwrap();
-        if line.len() % 2 != 0 {
-            panic!("Rucksack does not contains same number of items in each compartment");
-        }
-
-        let half_index = line.len() / 2;
-        let left_compartment: HashSet<u8> = HashSet::from_iter(line[..half_index].bytes());
-        let right_compartment: HashSet<u8> = HashSet::from_iter(line[half_index..].bytes());
-
-        let difference_sum: u32 = left_compartment
-            .intersection(&right_compartment)
-            .map(|c| match *c as char {
-                'a'..='z' => *c - ('a' as u8) + 1,
-                'A'..='Z' => *c - ('A' as u8) + 27,
-                _ => panic!("Unexcpected byte in input")
-            } as u32)
-            .sum();
-
-        total += difference_sum;
+    match args.mode {
+        Mode::Part01 => part_01(fp),
+        Mode::Part02 => part_02(fp),
     }
-
-    println!("Total is {}", total);
 }
