f9d37480e21c50e7d1bc56a17c505fe30a363b4e
Thomas Desveaux <desveaux.thomas@gmail.com>
2022-12-04 18:23:39 +0100

    [DAY04][Part Two] solution

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -16,22 +16,29 @@ struct Cli {
     mode: Mode,
 }
 
+fn get_ranges(line: String) -> ((i32, i32), (i32, i32)) {
+    let ranges = line
+        .split([',', '-'])
+        .map(|e| e.parse::<i32>().unwrap())
+        .tuples()
+        .collect::<Vec<(i32, i32)>>();
+    if ranges.len() != 2 {
+        panic!("unhandled not two ranges");
+    }
+
+    (ranges[0], ranges[1])
+}
+
+fn has_overlap(left: (i32, i32), right: (i32, i32)) -> bool {
+    left.0 <= right.1 && left.1 >= right.0
+}
+
 fn part_01(fp: File) {
     let lines = io::BufReader::new(fp).lines();
 
     let mut complete_overlap = 0;
     for l in lines {
-        let ranges = l.unwrap()
-            .split([',', '-'])
-            .map(|e| e.parse::<i32>().unwrap())
-            .tuples()
-            .collect::<Vec<(i32, i32)>>();
-        if ranges.len() != 2 {
-            panic!("unhandled not two ranges");
-        }
-
-        let (begin_1, end_1) = ranges[0];
-        let (begin_2, end_2) = ranges[1];
+        let ((begin_1, end_1), (begin_2, end_2)) = get_ranges(l.unwrap());
 
         let contains = ((begin_2 - begin_1) * (end_2 - end_1)) <= 0;
         if contains {
@@ -42,6 +49,17 @@ fn part_01(fp: File) {
 }
 
 fn part_02(fp: File) {
+    let lines = io::BufReader::new(fp).lines();
+
+    let mut overlap = 0;
+    for l in lines {
+        let (left, right) = get_ranges(l.unwrap());
+
+        if has_overlap(left, right) {
+            overlap += 1;
+        }
+    }
+    println!("Total overlap: {}", overlap);
 }
 
 fn main() {
@@ -57,5 +75,22 @@ fn main() {
     match args.mode {
         Mode::Part01 => part_01(fp),
         Mode::Part02 => part_02(fp),
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_has_overlap() {
+        assert_eq!(has_overlap((2, 4), (6, 8)), false);
+        assert_eq!(has_overlap((2, 3), (4, 5)), false);
+
+        assert_eq!(has_overlap((5, 7), (7, 9)), true);
+        assert_eq!(has_overlap((2, 8), (3, 7)), true);
+        assert_eq!(has_overlap((6, 6), (4, 6)), true);
+        assert_eq!(has_overlap((2, 6), (4, 8)), true);
     }
 }
