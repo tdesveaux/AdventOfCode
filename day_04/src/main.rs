2ccc4daf240beb894ea6ea552274679876293cdb
Thomas Desveaux <desveaux.thomas@gmail.com>
2022-12-04 19:49:54 +0100

    [DAY04] Refactor

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -16,7 +16,7 @@ struct Cli {
     mode: Mode,
 }
 
-fn get_ranges(line: String) -> ((i32, i32), (i32, i32)) {
+fn get_ranges(line: &String) -> ((i32, i32), (i32, i32)) {
     let ranges = line
         .split([',', '-'])
         .map(|e| e.parse::<i32>().unwrap())
@@ -29,37 +29,12 @@ fn get_ranges(line: String) -> ((i32, i32), (i32, i32)) {
     (ranges[0], ranges[1])
 }
 
-fn has_overlap(left: (i32, i32), right: (i32, i32)) -> bool {
+fn has_contains((left, right): ((i32, i32), (i32, i32))) -> bool {
+    ((right.0 - left.0) * (right.1 - left.1)) <= 0
+}
+
+fn has_overlap((left, right): ((i32, i32), (i32, i32))) -> bool {
     left.0 <= right.1 && left.1 >= right.0
-}
-
-fn part_01(fp: File) {
-    let lines = io::BufReader::new(fp).lines();
-
-    let mut complete_overlap = 0;
-    for l in lines {
-        let ((begin_1, end_1), (begin_2, end_2)) = get_ranges(l.unwrap());
-
-        let contains = ((begin_2 - begin_1) * (end_2 - end_1)) <= 0;
-        if contains {
-            complete_overlap += 1;
-        }
-    }
-    println!("Total contains: {}", complete_overlap);
-}
-
-fn part_02(fp: File) {
-    let lines = io::BufReader::new(fp).lines();
-
-    let mut overlap = 0;
-    for l in lines {
-        let (left, right) = get_ranges(l.unwrap());
-
-        if has_overlap(left, right) {
-            overlap += 1;
-        }
-    }
-    println!("Total overlap: {}", overlap);
 }
 
 fn main() {
@@ -72,10 +47,17 @@ fn main() {
     }
 
     let fp = File::open(file_path).unwrap();
-    match args.mode {
-        Mode::Part01 => part_01(fp),
-        Mode::Part02 => part_02(fp),
-    }
+    let filter_func = match args.mode {
+        Mode::Part01 => has_contains,
+        Mode::Part02 => has_overlap,
+    };
+
+    let total = io::BufReader::new(fp)
+        .lines()
+        .map(|l| l.unwrap())
+        .filter(|l| filter_func(get_ranges(l)))
+        .count();
+    println!("Total: {}", total);
 }
 
 
@@ -85,12 +67,12 @@ mod tests {
 
     #[test]
     fn test_has_overlap() {
-        assert_eq!(has_overlap((2, 4), (6, 8)), false);
-        assert_eq!(has_overlap((2, 3), (4, 5)), false);
+        assert_eq!(has_overlap(((2, 4), (6, 8))), false);
+        assert_eq!(has_overlap(((2, 3), (4, 5))), false);
 
-        assert_eq!(has_overlap((5, 7), (7, 9)), true);
-        assert_eq!(has_overlap((2, 8), (3, 7)), true);
-        assert_eq!(has_overlap((6, 6), (4, 6)), true);
-        assert_eq!(has_overlap((2, 6), (4, 8)), true);
+        assert_eq!(has_overlap(((5, 7), (7, 9))), true);
+        assert_eq!(has_overlap(((2, 8), (3, 7))), true);
+        assert_eq!(has_overlap(((6, 6), (4, 6))), true);
+        assert_eq!(has_overlap(((2, 6), (4, 8))), true);
     }
 }
