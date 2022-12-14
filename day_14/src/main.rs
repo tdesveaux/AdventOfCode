f5702092f8f7fc11734ac458a0d6b92904f1438e
Thomas Desveaux <thomas.desveaux@dont-nod.com>
2022-12-14 19:05:45 +0100

    [DAY14] optimize. Keep track of previous move to avoid recomputing

--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -1,5 +1,4 @@
-use std::{fs::File, io::{self, BufRead}, collections::HashSet, ops::RangeInclusive};
-
+use std::{fs::File, io::{self, BufRead}, collections::HashSet};
 
 fn parse() -> HashSet<(i64, i64)> {
     const SEPARATOR: &str = " -> ";
@@ -37,61 +36,23 @@ fn parse() -> HashSet<(i64, i64)> {
     blocked
 }
 
-fn print_state(y_r: RangeInclusive<i64>, x_r: RangeInclusive<i64>, blocked: &HashSet<(i64, i64)>, sandblocked: &HashSet<(i64, i64)>, c_pos: (i64, i64), floor_level: Option<i64>) {
-    let y_r = match floor_level {
-        Some(l) => *y_r.start()..=l,
-        None => y_r,
-    };
-    for y in y_r {
-        for x in x_r.clone() {
-            let pos = (x, y);
-            if Some(y) >= floor_level || blocked.contains(&pos) {
-                print!("#");
-            }
-            else if sandblocked.contains(&pos) {
-                print!("o");
-            }
-            else if pos == c_pos {
-                print!("~");
-            }
-            else {
-                print!(".");
-            }
-        }
-        println!();
-    }
-    println!();
-
-}
-
-
 fn main() {
     let blocked = parse();
 
     const SAND_ORIGIN: (i64, i64) = (500, 0);
 
-    let (ox, oy) = SAND_ORIGIN;
-
-    let min_x = blocked.iter().map(|(x, _y)| x).min().unwrap().min(&ox).to_owned() - 1;
-    let min_y = blocked.iter().map(|(_x, y)| y).min().unwrap().min(&oy).to_owned() - 1;
-    let max_x = blocked.iter().map(|(x, _y)| x).max().unwrap().max(&ox).to_owned() + 1;
-    let max_y = blocked.iter().map(|(_x, y)| y).max().unwrap().max(&oy).to_owned() + 1;
-
-    println!("{:?} {:?}", (min_x, min_y), (max_x, max_y));
+    let max_y = blocked.iter().map(|(_x, y)| y).max().unwrap().max(&SAND_ORIGIN.1).to_owned();
 
     let mut sandblocked = HashSet::new();
 
-    print_state(min_y..=max_y, min_x..=max_x, &blocked, &sandblocked, (i64::MAX, i64::MAX), None);
-
+    let mut passage = vec![];
     'outer: loop {
-        let (mut sx, mut sy) = SAND_ORIGIN;
+        let (mut sx, mut sy) = passage.pop().unwrap_or(SAND_ORIGIN);
 
         loop {
-            if sy > max_y {
+            if sy >= max_y {
                 break 'outer;
             }
-
-            // print_state(min_y..=max_y, min_x..=max_x, &blocked, &sandblocked, (sx, sy));
 
             // go down
             let candidates = [
@@ -99,8 +60,11 @@ fn main() {
                 (sx - 1, sy + 1), // down-left
                 (sx + 1, sy + 1), // down-right
             ];
-            match candidates.iter().find(|c| !blocked.contains(&c) && !sandblocked.contains(&c)) {
-                Some(c) => (sx, sy) = *c,
+            match candidates.iter().find(|&c| !blocked.contains(c) && !sandblocked.contains(c)) {
+                Some(c) => {
+                    passage.push((sx, sy));
+                    (sx, sy) = *c
+                }
                 None => {
                     // found resting place
                     sandblocked.insert((sx, sy));
@@ -110,14 +74,12 @@ fn main() {
         }
     }
 
-    print_state(min_y..=max_y, min_x..=max_x, &blocked, &sandblocked, (i64::MAX, i64::MAX), None);
     println!("Going into the abyss. {}", sandblocked.len());
 
-    // Reset sand
-    sandblocked = HashSet::new();
-    let floor_level = blocked.iter().map(|(_x, y)| y).max().unwrap().max(&oy).to_owned() + 2;
+    let floor_level = max_y + 2;
+
     'outer: loop {
-        let (mut sx, mut sy) = SAND_ORIGIN;
+        let (mut sx, mut sy) = passage.pop().unwrap_or(SAND_ORIGIN);
 
         loop {
             // go down
@@ -127,7 +89,10 @@ fn main() {
                 (sx + 1, sy + 1), // down-right
             ];
             match candidates.iter().filter(|(_x, y)| *y < floor_level).find(|c| !blocked.contains(&c) && !sandblocked.contains(&c)) {
-                Some(c) => (sx, sy) = *c,
+                Some(c) => {
+                    passage.push((sx, sy));
+                    (sx, sy) = *c
+                }
                 None => {
                     // found resting place
                     sandblocked.insert((sx, sy));
@@ -140,11 +105,5 @@ fn main() {
         }
     }
 
-    let min_x = sandblocked.iter().map(|(x, _y)| x).min().unwrap().min(&min_x).to_owned() - 1;
-    let min_y = sandblocked.iter().map(|(_x, y)| y).min().unwrap().min(&min_y).to_owned();
-    let max_x = sandblocked.iter().map(|(x, _y)| x).max().unwrap().max(&max_x).to_owned() + 1;
-    let max_y = sandblocked.iter().map(|(_x, y)| y).max().unwrap().max(&max_y).to_owned() + 1;
-
-    print_state(min_y..=max_y, min_x..=max_x, &blocked, &sandblocked, (i64::MAX, i64::MAX), Some(floor_level));
     println!("max count {}", sandblocked.len());
 }
