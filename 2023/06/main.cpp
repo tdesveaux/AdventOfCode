58bc105a6a1462513b011412960b96afa907efa8
Thomas Desveaux <thomas.desveaux@dont-nod.com>
2023-12-08 20:24:02 +0100

    [2023][DAY06][P2]

--- 2023/06/main.cpp
+++ 2023/06/main.cpp
@@ -6,6 +6,21 @@
 
 using namespace std;
 
+long count_success_possibilities(long time, long distance_record)
+{
+    for (long time_spent = 0; time_spent <= time; ++time_spent)
+    {
+        const auto distance = time_spent * (time - time_spent);
+        // printf("\t %ld -> %ld (vs %ld)\n", time_spent, distance, distance_record);
+        if (distance > distance_record)
+        {
+            return (time - time_spent + 1) - time_spent;
+        }
+    }
+
+    return 0;
+}
+
 int main(int argc, char** argv)
 {
     if (argc != 2)
@@ -37,29 +52,26 @@ int main(int argc, char** argv)
 
     int part1_prod = 1;
 
+    string part2_time_s;
+    string part2_distance_record_s;
+
     const auto end_it = std::sregex_iterator();
     for (; times_it != end_it && distances_it != end_it; ++times_it, ++distances_it)
     {
-        const auto time = stoi((*times_it).str());
-        const auto distance_record = stoi((*distances_it).str());
+        const auto time_s = (*times_it).str();
+        const auto distance_record_s = (*distances_it).str();
+
+        part2_time_s += time_s;
+        part2_distance_record_s += distance_record_s;
+
+        const auto time = stoi(time_s);
+        const auto distance_record = stoi(distance_record_s);
 
         printf("%d -> %d\n", time, distance_record);
 
         int min_time = (distance_record / time);
 
-        int race_success_ways_count = 0;
-
-        for (int time_spent = 0; time_spent <= time; ++time_spent)
-        {
-            const auto distance = time_spent * (time - time_spent);
-            printf("\t %d -> %d (vs %d)\n", time_spent, distance, distance_record);
-            if (distance > distance_record)
-            {
-                race_success_ways_count = (time - time_spent + 1) - time_spent;
-                break;
-            }
-        }
-
+        int race_success_ways_count = count_success_possibilities(time, distance_record);
         if (race_success_ways_count > 0)
         {
             part1_prod *= race_success_ways_count;
@@ -67,6 +79,12 @@ int main(int argc, char** argv)
     }
 
     printf("Part 01, ways to beat record: %d\n", part1_prod);
+
+    const auto part2_time = stol(part2_time_s);
+    const auto part2_distance_record = stol(part2_distance_record_s);
+
+    int part2_success_ways_count = count_success_possibilities(part2_time, part2_distance_record);
+    printf("Part 02, ways to beat record: %ld\n", part2_success_ways_count);
 
     return 0;
 }
