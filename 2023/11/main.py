10c291d254eb750490d6bdcb1519487f9f86c375
Thomas Desveaux <thomas.desveaux@dont-nod.com>
2023-12-27 16:21:17 +0100

    [2023][DAY11][P2]

--- 2023/11/main.py
+++ 2023/11/main.py
@@ -26,9 +26,10 @@ empty_lines = set(range(length)).difference(non_empty_lines)
 empty_columns = set(range(length)).difference(non_empty_columns)
 
 # move points with deduced empty spaces
+coeff = 1000000
 for p in points:
-    p.x += sum(p.x > e for e in empty_columns)
-    p.y += sum(p.y > e for e in empty_lines)
+    p.x += sum(p.x > e for e in empty_columns) * (coeff - 1)
+    p.y += sum(p.y > e for e in empty_lines) * (coeff - 1)
 
 y_len = max(p.y for p in points)
 x_len = max(p.x for p in points)
@@ -37,11 +38,11 @@ def manhattan_length(left: Point, right: Point):
     return abs(right.x - left.x) + abs(right.y - left.y)
 
 
-part_1 = sum(
+part_2 = sum(
     manhattan_length(left, right)
     for l_idx, left in enumerate(points)
     for right in points[l_idx+1:]
 )
 
 
-print(f"part 1: {part_1}")
+print(f"part 2: {part_2}")
